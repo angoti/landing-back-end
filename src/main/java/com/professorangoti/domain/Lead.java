0210f3b982b294514c8cc6be67b41cf72883b613
Edson Angoti Júnior <angoti@gmail.com>
2022-06-29 12:15:49 -0300

    @GeneratedValue(strategy = GenerationType.AUTO)

--- src/main/java/com/professorangoti/domain/Lead.java
+++ src/main/java/com/professorangoti/domain/Lead.java
@@ -11,7 +11,7 @@ import lombok.Data;
 @Data
 public class Lead {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String nome, email;
 }
