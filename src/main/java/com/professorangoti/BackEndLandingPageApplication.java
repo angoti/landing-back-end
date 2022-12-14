5f601be2d20e041eda48433e2fac4db25bb2087d
Edson Angoti Junior <angoti@gmail.com>
2022-12-14 19:49:22 +0000

    heroku debug

--- src/main/java/com/professorangoti/BackEndLandingPageApplication.java
+++ src/main/java/com/professorangoti/BackEndLandingPageApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackEndLandingPageApplication {
+public class BackEndLandingPageApplication implements CommandLineRunner  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackEndLandingPageApplication.class, args);
